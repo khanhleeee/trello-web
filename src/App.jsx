436493a4873ad05eb22a7555cdd4a347465e2034
khanhleeee <khanhleee.itvn@gmail.com>
2023-07-06 19:37:25 +0700

    refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import Button from '@mui/material/Button'
-import AccessAlarm from '@mui/icons-material/AccessAlarm'
+import Boards from './pages/Boards/_id'
 
 
 function App() {
-  
   return (
-    <>
-      <AccessAlarm />
-      <h2>Hello</h2>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
+    <Boards />
   )
 }
 
---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,57 @@
+import { useColorScheme } from '@mui/material'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+
+import Box from '@mui/material/Box'
+
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
+
+
+const ModeSelect = () => {
+
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="demo-select-small"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessOutlinedIcon fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default ModeSelect
